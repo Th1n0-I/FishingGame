9e87d6ad5a9e043c78d644a2d3af52a53d19817b
Thino Ivarsson <thinoivarsson7@gmail.com>
2026-06-28 00:34:37 +0200

    feat: Started porting the volumetrics shader to a compute shader

--- Assets/Scripts/Noise/NoiseController.cs
+++ Assets/Scripts/Noise/NoiseController.cs
@@ -1,30 +1,48 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class NoiseController : MonoBehaviour {
 	
+	[SerializeField] private int           textureDivide = 2;
+	
 	[SerializeField] private int seed = 0;
 	
-	[SerializeField] private ComputeShader shader;
-	[SerializeField] private RenderTexture renderTexture;
+	[SerializeField] private ComputeShader noiseShader, volumetricsShader;
+	[SerializeField] private RenderTexture noiseRenderTexture, volumetricsRenderTexture;
 
 	[SerializeField] private bool onRawImage;
 	[SerializeField] private int  dotAmount;
 	[SerializeField, Range(0,1)] private float z;
 	
 	private RawImage rawImage;
-	
+
+	private void OnEnable() {
+		RenderPipelineManager.endContextRendering += OnEndFrameRendering;
+	}
+
+	private void OnDisable() {
+		RenderPipelineManager.endContextRendering -= OnEndFrameRendering;
+	}
 
 	private void Start() {
 		
 		
 		
-		renderTexture                   = new RenderTexture(128, 128, 0, RenderTextureFormat.ARGBFloat);
-		renderTexture.dimension         = UnityEngine.Rendering.TextureDimension.Tex3D;
-		renderTexture.volumeDepth       = 128;
-		renderTexture.enableRandomWrite = true;
-		renderTexture.wrapMode = TextureWrapMode.Repeat;
-		renderTexture.Create();
+		noiseRenderTexture                   = new RenderTexture(128, 128, 0, RenderTextureFormat.ARGBFloat);
+		noiseRenderTexture.dimension         = UnityEngine.Rendering.TextureDimension.Tex3D;
+		noiseRenderTexture.volumeDepth       = 128;
+		noiseRenderTexture.enableRandomWrite = true;
+		noiseRenderTexture.wrapMode = TextureWrapMode.Repeat;
+		noiseRenderTexture.Create();
+		
+		volumetricsRenderTexture = new RenderTexture(Screen.width / textureDivide, Screen.height / textureDivide, 24);
+		volumetricsRenderTexture.enableRandomWrite = true;
+		volumetricsRenderTexture.wrapMode = TextureWrapMode.Repeat;
+		volumetricsRenderTexture.Create();
+		
 		
 		Vector3[] dots = new Vector3[dotAmount * dotAmount * dotAmount];
 		
@@ -48,24 +66,37 @@ public class NoiseController : MonoBehaviour {
 			longDots[i] = new Vector4(dots[i].x, dots[i].y ,dots[i].z,1);
 		}
 		
-		shader.SetTexture(0, "Result", renderTexture);
-		shader.SetFloat("Resolution", renderTexture.width);
-		shader.SetVectorArray("Points", longDots);
-		shader.SetInt("PointCount", dotAmount *  dotAmount);
-		shader.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, renderTexture.volumeDepth / 8);
-		Shader.SetGlobalTexture("_NoiseTex", renderTexture);
+		noiseShader.SetTexture(0, "Result", noiseRenderTexture);
+		noiseShader.SetFloat("Resolution", noiseRenderTexture.width);
+		noiseShader.SetVectorArray("Points", longDots);
+		noiseShader.SetInt("PointCount", dotAmount *  dotAmount);
+		noiseShader.Dispatch(0, noiseRenderTexture.width / 8, noiseRenderTexture.height / 8, noiseRenderTexture.volumeDepth / 8);
+		Shader.SetGlobalTexture("_NoiseTex", noiseRenderTexture);
+		
+		volumetricsShader.SetTexture(0, "NoiseTex", noiseRenderTexture);
+		volumetricsShader.SetTexture(0, "Result", volumetricsRenderTexture);
+		
+		Shader.SetGlobalTexture("VolumetricsTex", volumetricsRenderTexture);
+		volumetricsShader.SetInt("TextureDivide", textureDivide);
+		volumetricsShader.SetInt("ScreenWidth", Screen.width);
+		volumetricsShader.SetInt("ScreenHeight", Screen.height);
 		
 		if (onRawImage) {
-			rawImage = GetComponent<RawImage>();
+			rawImage = GetComponentInChildren<RawImage>();
 			//rawImage.texture = renderTexture;
 		}
 	}
 
-	private void Update() {
-		
-		if (onRawImage) {
-			//rawImage.texture = renderTexture;
-		}
-	} 
+	private void OnEndFrameRendering(ScriptableRenderContext context, List<Camera> cams) {
 
+		var tex = Shader.GetGlobalTexture("_CameraDepthTexture");
+		Debug.Log(tex);
+		volumetricsShader.SetTexture(0, "DepthTex", tex);
+		volumetricsShader.Dispatch(0,volumetricsRenderTexture.width / 8, volumetricsRenderTexture.height / 8, 1);
+
+		if (onRawImage) {
+			rawImage.texture = volumetricsRenderTexture;
+			rawImage.texture = Shader.GetGlobalTexture("_CameraDepthTexture");
+		}
+	}
 }
